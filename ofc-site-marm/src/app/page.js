49440b06a696ea1ffbbe770f89e4efb6a0625a26
Gabriel Constante Schlichting <constanteg984@gmail.com>
2024-11-03 22:03:49 -0300

    add next
    next add

---
+++ ofc-site-marm/src/app/page.js
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./page.module.css"
+
+
+export default function Home() {
+  return (
+    <div className="cabeca">
+<nav className="nav-bar">
+  
+<Link href={'./contatos'} className="link-contatos"><button className="btn-donate">contatos</button></Link>
+
+
+</nav>
+
+
+
+
+
+    </div>
+  );
+}
