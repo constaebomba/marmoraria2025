a02a6889a8fb0b0d8baef498b0dbfa5cae1cac35
Gabriel Constante Schlichting <constanteg984@gmail.com>
2024-11-04 16:28:30 -0300

    oioi
    tchauu

--- ofc-site-marm/src/app/page.js
+++ ofc-site-marm/src/app/page.js
@@ -1,17 +1,17 @@
 import Image from "next/image";
 import Link from "next/link";
-import styles from "./styles.modules.css"
+import styles from "./globals.css";
 
 export default function Home() {
   return (
   
-<section>
-
+<div>
 <div className="head">
 
-<h1 className="tittle">Marmoraria Florianopolis</h1>
+<h1 className="tittle">Marmoraria Florianópolis</h1>
 
 </div>
+
     <div className="barra">
       <Link href={'./empresa'} className="link-sobre"><button className="btn">EMPRESA</button></Link>
       <Link href={'./materiais'} className="link-serviços"><button className="btn">MATERIAIS</button></Link>
@@ -20,10 +20,10 @@ export default function Home() {
     </div>
     
              <div className="corpo">
-             <img src="/back.marm.jpg" alt="Descrição da imagem" className="back-marm"/>  
-             <img src="/row.png" alt="Descrição da imagem" className="row-marm"/>  
-             </div>
+  <img src="/back.marm.jpg"  className="back-marm"/>  
+           
+</div>
+</div>
 
-</section>
 )
 }
