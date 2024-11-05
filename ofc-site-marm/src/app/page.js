71639b0af8182bc605790c0cb4e2113cb32da6a0
Gabriel Constante Schlichting <constanteg984@gmail.com>
2024-11-05 02:10:54 -0300

    add rowller
    rowller add congratulations

--- ofc-site-marm/src/app/page.js
+++ ofc-site-marm/src/app/page.js
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
 import styles from "./globals.css";
+import Styles from "./styles.modules.css"
+
 
 export default function Home() {
   return (
-  
-<div>
+    <section>
 <div className="head">
 
 <h1 className="tittle">Marmoraria Florianópolis</h1>
@@ -16,14 +17,24 @@ export default function Home() {
       <Link href={'./empresa'} className="link-sobre"><button className="btn">EMPRESA</button></Link>
       <Link href={'./materiais'} className="link-serviços"><button className="btn">MATERIAIS</button></Link>
       <Link href={'./trabalhos'} className="link-trabalhos"><button className="btn">TRABALHOS</button></Link>
+      <Link href={'./contatos'} className="link-parceiros"><button className="btn">PARCEIROS</button></Link>
       <Link href={'./contatos'} className="link-contatos"><button className="btn">CONTATOS</button></Link>
     </div>
-    
-             <div className="corpo">
-  <img src="/back.marm.jpg"  className="back-marm"/>  
-           
-</div>
-</div>
+<main className="rowller">
+  <img src="/back-premium.png" alt="desc" className="back-premium"></img>
+  <div className="photos4">
+          <img src="/row-1.png" alt="desc" className="row-1"></img>  
+          <img src="/row-2.png" alt="desc" className="row-2"></img>  
+          <img src="/row-3.png" alt="desc" className="row-3"></img>  
+          <img src="/row-4.png" alt="desc" className="row-4"></img>  
+  </div>         
+</main>
+
+  <div>
+  </div>
+         
+
+      </section>          
 
 )
 }
